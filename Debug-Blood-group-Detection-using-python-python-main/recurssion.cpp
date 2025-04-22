3b770f89de015d8532011a89a62ffbb17bff835a
unknown <www.sahanapresidency@gmail.com>
2025-04-22 07:05:25 +0530

    Blood

---
+++ Debug-Blood-group-Detection-using-python-python-main/recurssion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespave std;
+void print(int n)
+{
+    if(n==0)
+    return;
+    print(n-1);
+    std::cout<<"I LOVE RECURSSION";
+
+int main()
+cin>>n;
+print(n);
+return 0;
+
+}
